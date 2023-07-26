565642ad7463e558d546a483633713e5a0e24312
Ariadne Pinheiro <ariadnepinheiro@id.uff.br>
2023-07-26 22:56:31 +0000

    Adicionado AppArrays.java

---
+++ src/main/java/com/example/AppArrays.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+
+        x = 20;
+        System.out.println(x);
+
+        double vetor[] = {10, 20, 30};
+        System.out.println(Arrays. toString(vetor));
+    }
+}
