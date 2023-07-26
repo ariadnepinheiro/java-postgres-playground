72666927dd94cad5eaa170e8a3804d3710fb2791
Ariadne Pinheiro <ariadnepinheiro@id.uff.br>
2023-07-26 23:38:06 +0000

    Práticas da Aula 5

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -10,7 +10,34 @@ public class AppArrays {
         x = 20;
         System.out.println(x);
 
-        double vetor[] = {10, 20, 30};
-        System.out.println(Arrays. toString(vetor));
+        double vetorDouble[] = {10, 20, 30};
+        System.out.println(Arrays. toString(vetorDouble));
+
+        System.out.println("Valor da posição 0: " + vetorDouble[0]);
+        System.out.println("Valor da posição 1: " + vetorDouble[1]);
+        System.out.println("Valor da posição 2: " + vetorDouble[2]);
+
+        vetorDouble[0] = 1;
+        System.out.println("Valor da posição 0: " + vetorDouble[0]);
+
+        int vetor3[] = new int[4];
+        vetor3[0] = 100;
+        vetor3[1] = 200;
+        vetor3[2] = 300;
+        vetor3[3] = 400;
+        System.out.println(Arrays.toString(vetor3));
+
+        double matriz[][] = { {10,20,30}, {40,50,60} };
+
+
+
+
+
+
+
+
+
+
+
     }
 }
