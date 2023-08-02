be9a1918c0f52232eb7a6437b53fad6ef8e438b7
Ariadne Pinheiro <ariadnepinheiro@id.uff.br>
2023-08-02 21:02:07 +0000

    Laço de Repetição for e uso com arrays

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class AppArrays {
     public static void main(String[] args) {
+        /*
         int x = 10;
         System.out.println(x);
 
@@ -11,7 +12,7 @@ public class AppArrays {
         System.out.println(x);
 
         double vetorDouble[] = {10, 20, 30};
-        System.out.println(Arrays. toString(vetorDouble));
+        System.out.println(Arrays.toString(vetorDouble));
 
         System.out.println("Valor da posição 0: " + vetorDouble[0]);
         System.out.println("Valor da posição 1: " + vetorDouble[1]);
@@ -20,18 +21,29 @@ public class AppArrays {
         vetorDouble[0] = 1;
         System.out.println("Valor da posição 0: " + vetorDouble[0]);
 
-        int vetor3[] = new int[4];
-        vetor3[0] = 100;
-        vetor3[1] = 200;
-        vetor3[2] = 300;
-        vetor3[3] = 400;
+        int vetor3[] = new int[5];
+        for (int i = 0; i < vetor3.length; i++) {
+            vetor3[i] = 100 * (i + 1);
+        }
         System.out.println(Arrays.toString(vetor3));
-
-        double matriz[][] = { {10,20,30}, {40,50,60} };
+        
         matriz[0][1] = -20;
         matriz[1] = new double[]{70,80};
         System.out.println(Arrays.toString(matriz[0]));
         System.out.println(Arrays.toString(matriz[1]));
+        */
 
+        double matriz[][] = new double[3][4];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (i * matriz[i].length + j+1) * 10;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%5.1f ", matriz[i][j]);
+            }
+        }
     }
 }
